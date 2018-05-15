967beaf2a720d44c1ef682ba70a48092524caf7f
Fiederling <MFD@salt-solutions.de>
2018-05-15 12:04:13 +0200

    Changed some values and added a description to Fire-Spawn-Timer

--- config.lua
+++ config.lua
@@ -1,14 +1,14 @@
 Config = {}
 
-Config.FireSpawnTimer = {
+Config.FireSpawnTimer = {   -- min/max time in seconds
   ["minTime"] = 2400, ["maxTime"] = 3600
 }
 
-Config.autoDeleteFireTimer = 15   -- time after which the fire will be deletet automacicly
+Config.autoDeleteFireTimer = 1200   -- time in seconds after which the fire will be deletet automacicly
 
 Config.fireStackHeight = 0.4    -- space between stacked flames
 
 Config.fireLocations = { -- x, y, z, minSpread, maxSpread, flameCount, fireType
-  {["x"]=1959.8701, ["y"]=3748.1833,  ["z"]=31.3437,  ["minSpread"]=-6,  ["maxSpread"]=6, ["flames"]=200, ["label"]="Ladenbrand"},       --  Sandy Shores Shop near Petrol Station
-  {["x"]=1853.7718, ["y"]=3746.4409,  ["z"]=32.3951,  ["minSpread"]=-6,  ["maxSpread"]=6, ["flames"]=200, ["label"]="Ladenbrand"},       --  Sandy Shores Tatoo Shop (needs to be further optimized)
+  {["x"]=1959.8701, ["y"]=3748.1833,  ["z"]=31.3437,  ["minSpread"]=-6,  ["maxSpread"]=6, ["flames"]=200, ["label"]="Ladenbrand"},--  Sandy Shores Shop near Petrol Station
+  {["x"]=1853.7718, ["y"]=3746.4409,  ["z"]=32.3951,  ["minSpread"]=-6,  ["maxSpread"]=6, ["flames"]=200, ["label"]="Ladenbrand"},--  Sandy Shores Tatoo Shop (needs to be further optimized)
 }
