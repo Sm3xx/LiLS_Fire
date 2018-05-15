a5260e5b713399434ea3ad45609dba9d83bc051c
Fiederling <MFD@salt-solutions.de>
2018-05-15 12:16:48 +0200

    Formated Comments

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 Config = {}
 
-Config.FireSpawnTimer = {   -- min/max time in seconds
+Config.FireSpawnTimer = {                 -- min/max time in seconds
   ["minTime"] = 2400, ["maxTime"] = 3600
 }
 
-Config.autoDeleteFireTimer = 1200   -- time in seconds after which the fire will be deletet automacicly
+Config.autoDeleteFireTimer = 1200         -- time in seconds after which the fire will be deletet automacicly
 
-Config.fireStackHeight = 0.35    -- space between stacked flames
+Config.fireStackHeight = 0.35             -- space between stacked flames
 
 Config.fireLocations = { -- x, y, z, minSpread, maxSpread, flameCount, fireType
   {["x"]=1959.8701, ["y"]=3748.1833,  ["z"]=31.3437,  ["minSpread"]=-6,  ["maxSpread"]=6, ["flames"]=200, ["label"]="Ladenbrand"},--  Sandy Shores Shop near Petrol Station
