1fddcbc7b853142550e556ea38d80ac0fff53cb2
Fiederling <MFD@salt-solutions.de>
2018-05-15 12:09:13 +0200

    Edited Stack-Height Value

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ Config.FireSpawnTimer = {   -- min/max time in seconds
 
 Config.autoDeleteFireTimer = 1200   -- time in seconds after which the fire will be deletet automacicly
 
-Config.fireStackHeight = 0.4    -- space between stacked flames
+Config.fireStackHeight = 0.35    -- space between stacked flames
 
 Config.fireLocations = { -- x, y, z, minSpread, maxSpread, flameCount, fireType
   {["x"]=1959.8701, ["y"]=3748.1833,  ["z"]=31.3437,  ["minSpread"]=-6,  ["maxSpread"]=6, ["flames"]=200, ["label"]="Ladenbrand"},--  Sandy Shores Shop near Petrol Station
